7d1520ab6dec665524793d1cc23e0ebfee42f85c
pjsillescas <pjsillescas@gmail.com>
2024-12-22 21:30:10 +0100

    Código de error 404 si no existe la ruta en el backend.

--- backend/index.php
+++ backend/index.php
@@ -22,6 +22,26 @@ $jsonMiddleware = function (Request $request, RequestHandler $handler)
 
 $app = AppFactory::create();
 $app->add($jsonMiddleware);
+
+/**
+ * The routing middleware should be added earlier than the ErrorMiddleware
+ * Otherwise exceptions thrown from it will not be handled by the middleware
+ */
+$app->addRoutingMiddleware();
+
+/**
+ * Add Error Middleware
+ *
+ * @param bool                  $displayErrorDetails -> Should be set to false in production
+ * @param bool                  $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool                  $logErrorDetails -> Display error details in error log
+ * @param LoggerInterface|null  $logger -> Optional PSR-3 Logger  
+ *
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
 $app->get("/", function (Request $request, Response $response, array $args)
 {
 	$data = array("data" => "todo");
